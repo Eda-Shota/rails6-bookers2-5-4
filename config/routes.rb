269a58cdef562d77f53ad5a8e8fc137948d47e19
EC2 Default User <ec2-user@ip-172-31-33-80.ap-northeast-1.compute.internal>
2023-01-11 04:45:28 +0000

    [Finish]5-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  devise_for :users
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
+    #このように親子関係を持たせることで /books/:book_id/book_commentsの形のURLになり、params[:book_id]Bookのidが取得できる。
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index,:show,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
 end
